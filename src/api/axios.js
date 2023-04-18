adb0de58672219f0528c647bd5b2d8a1b44ab57a
seonwooke <seonookkim@gmail.com>
2023-04-18 17:12:28 +0900

    :art: Improve Structure
    Improve Structure

---
+++ src/api/axios.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const instance = axios.create({
+    baseURL: "http://api.themoviedb.org/3",
+    param: {
+        api_key : "69c5c00f36fe5c19b5a6c65a8d9c7941",
+        language: "ko-KR" 
+    }
+});
+
+export default instance;
