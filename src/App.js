0da30f86ff8967de27c5f1172c8aaa723b90c4e5
seonwooke <seonookkim@gmail.com>
2023-04-18 23:13:04 +0900

    :zap: Nav&Banner&Category&Row Dev
    Nav&Banner&Category&Row Dev

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import requests from './api/request';
+import Banner from './components/Banner';
+import Category from './components/Category';
+import Nav from './components/Nav';
+import Row from './components/Row';
+import styled from 'styled-components';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+      <Container>
+        <Nav />
+        <Banner />
+        <Category />
+        <Row title="Tranding Now" id="TN" fetchUrl={requests.fetchTrending} />
+        <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated} />
+        <Row title="Action Movies" id="AM" fetchUrl={requests.fetchActionMovies} />
+        <Row title="Comedy Movies" id="CM" fetchUrl={requests.fetchComedyMovies} />
+      </Container>
+    );
 }
 
 export default App;
+
+const Container = styled.main`
+    position: relative;
+    min-height: calc(100vh - 250px);
+    overflow-x: hidden;
+    display: block;
+    top: 72px;
+    padding: 0 calc(3.5vw + 5px);
+
+    &:after {
+        background: url("/images/home-background.png") center center / cover no-repeat fixed;
+        content: "";
+        position: absolute;
+        inset: 0px;
+        opacity: 1;
+        z-index: -1;
+    }
+`
--- src/api/axios.js
+++ src/api/axios.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const instance = axios.create({
     baseURL: "http://api.themoviedb.org/3",
-    param: {
-        api_key : "69c5c00f36fe5c19b5a6c65a8d9c7941",
+    params: {
+        api_key: "69c5c00f36fe5c19b5a6c65a8d9c7941",
         language: "ko-KR" 
     }
 });
